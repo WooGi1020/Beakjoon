13236630f436ff4a641840b4f8a68d9ca2f15c39
우기 <eric0584@hs.ac.kr>
2023-07-06 23:52:23 +0900

    20230706

---
+++ 백준/code tree/프로그래밍 연습/특정 구간의 합.py
@@ -0,0 +1,14 @@
+n, m = map(int,input().split())
+
+mul1 = [0] + list(map(int,input().split()))
+
+def cal(a,b):
+    total
+    for i in range(a,b+1):
+        total += mul[i]
+    return total
+
+i = 0
+while i<m:
+    num1, num2 = map(int, input().split())
+    
