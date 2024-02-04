a6464da428568a66c5caa10a230be222c7f6bdb5
우기 <jungwk1020@gmail.com>
2024-02-04 18:41:01 +0900

    20230204

--- Front-End-Prc/HTML+CSS+JS/JS_Study/JS_Prc/JS_Study_Lorem_Maker/script.js
+++ Front-End-Prc/HTML+CSS+JS/JS_Study/JS_Prc/JS_Study_Lorem_Maker/script.js
@@ -8,7 +8,7 @@ const loremArray = [
   molestie nulla eu dignissim. Quisque ut diam ut ligula porta hendrerit. 
   Phasellus ac maximus nunc. Donec tempus, leo nec imperdiet sodales, nisi diam 
   placerat tortor, et bibendum velit nisi quis urna. Phasellus vestibulum mauris
-  a dolor ullamcorper, eu tristique ipsum convallis.\n\n`,
+  a dolor ullamcorper, eu tristique ipsum convallis.`,
   `Vestibulum ut interdum est. Morbi in ultrices dui. Suspendisse dui erat, 
   pulvinar ut pellentesque ut, bibendum sed turpis. Aenean ipsum tellus, elementum 
   eget rhoncus viverra, tempor quis arcu. Nunc egestas vel velit in fermentum.
@@ -16,13 +16,13 @@ const loremArray = [
   conubia nostra, per inceptos himenaeos. Vivamus metus odio, condimentum id est 
   in, scelerisque facilisis lectus. Integer consequat ligula nibh. Nullam a 
   enim non sapien suscipit aliquet. Ut blandit erat nisl, vitae porta lorem 
-  vestibulum nec. Fusce vitae ipsum a diam posuere consectetur.\n\n`,
+  vestibulum nec. Fusce vitae ipsum a diam posuere consectetur.`,
   `Phasellus in porta dolor, at malesuada velit. Nam vestibulum eleifend egestas. 
   Phasellus eget posuere tellus, vitae accumsan tellus. Aliquam tortor nisi, 
   dignissim eget libero placerat, efficitur mattis augue. Mauris quis elit odio.
   Fusce tortor dui, ultrices et dui vehicula, mattis varius ex. Maecenas ullamcorper
   quam sit amet ullamcorper ullamcorper. Duis convallis tempus odio, nec fringilla
-  nibh auctor in.\n\n`,
+  nibh auctor in.`,
   `Curabitur fermentum augue et eleifend iaculis. Nulla condimentum, ex at feugiat
    viverra, arcu nisi tincidunt felis, sit amet dapibus augue lacus eget mauris. 
    Pellentesque sodales dolor consequat magna vehicula, in sagittis nibh volutpat. 
@@ -33,7 +33,7 @@ const loremArray = [
    sit amet urna vestibulum rutrum. Donec porta lorem ligula, vel pulvinar nunc 
    dapibus in. Donec lobortis, erat et molestie dignissim, purus felis accumsan nisi, 
    eget porta nisi turpis id tortor. Interdum et malesuada fames ac ante ipsum primis 
-   in faucibus. Sed turpis nisl, commodo ac enim eu, finibus lobortis leo.\n\n`,
+   in faucibus. Sed turpis nisl, commodo ac enim eu, finibus lobortis leo.`,
    `Ut sit amet aliquam nibh. Sed eu consectetur arcu, non mollis enim. Morbi sit 
    amet fermentum massa. Nulla et magna purus. Quisque tincidunt libero tempor 
    lorem consectetur, ac iaculis orci dignissim. Praesent interdum justo fringilla, 
@@ -42,7 +42,7 @@ const loremArray = [
    nec consequat dui convallis eget. Nulla dolor dui, fringilla vitae nunc sed, 
    sollicitudin posuere est. Orci varius natoque penatibus et magnis dis parturient 
    montes, nascetur ridiculus mus. Vivamus porttitor at magna vel malesuada. Maecenas 
-   sodales urna a rutrum feugiat.\n\n`,
+   sodales urna a rutrum feugiat.`,
    `Sed gravida mauris viverra ligula fringilla ornare. In in lectus metus. 
    Aliquam sapien mi, laoreet ac nisi sed, volutpat semper est. Aenean lacus leo, 
    maximus id mattis id, imperdiet non diam. Nullam condimentum tellus sit amet 
@@ -53,14 +53,14 @@ const loremArray = [
    vulputate, accumsan luctus turpis. Mauris maximus nunc sit amet velit auctor 
    vestibulum. Morbi tempus ligula vitae velit tempus feugiat sed non eros. Maecenas 
    scelerisque nunc mi, id volutpat enim vulputate nec. In in ex a dolor consequat 
-   feugiat nec id justo. Aliquam feugiat dolor non accumsan convallis.\n\n`,
+   feugiat nec id justo. Aliquam feugiat dolor non accumsan convallis.`,
    `Morbi auctor velit quis nisi rhoncus faucibus. Duis ornare ultrices fermentum. 
    Quisque non elit sed sem fermentum aliquet in in augue. Curabitur vitae gravida 
    augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque non leo 
    ac ante gravida hendrerit ut ac leo. In hac habitasse platea dictumst. Nam 
    pulvinar, nulla vitae placerat pellentesque, augue velit iaculis arcu, ac accumsan 
    ex purus at velit. Cras bibendum elit eu purus mollis rutrum vel sed lacus. Cras 
-   posuere aliquet venenatis. Nam faucibus bibendum accumsan.\n\n`,
+   posuere aliquet venenatis. Nam faucibus bibendum accumsan.`,
    `Aliquam vitae eros ut mauris fermentum venenatis quis non lorem. Donec ut ante 
    ac risus vulputate convallis nec eu mauris. Donec finibus iaculis blandit. 
    Phasellus tellus nunc, facilisis eget semper sed, vulputate non metus. Praesent 
@@ -71,7 +71,7 @@ const loremArray = [
    mollis dolor sed, semper justo. Pellentesque convallis vitae purus a interdum. 
    Praesent sed volutpat sem. Duis vehicula, elit a luctus ultricies, tortor ligula 
    mollis tellus, sit amet dapibus neque diam sollicitudin lectus. Nullam at auctor 
-   tellus.\n\n`,
+   tellus.`,
    `Vestibulum efficitur, enim sit amet mattis viverra, lectus orci ultricies 
    sapien, vitae aliquam massa lacus sit amet risus. Orci varius natoque penatibus 
    et magnis dis parturient montes, nascetur ridiculus mus. Duis commodo vitae metus 
@@ -82,7 +82,7 @@ const loremArray = [
    dui molestie convallis sed at leo. Quisque maximus tempus luctus. Nam fermentum 
    nisl vitae tempor porttitor. Integer hendrerit ligula in magna venenatis vulputate. 
    Fusce eget lacus condimentum lorem venenatis tincidunt. Vestibulum quis dolor 
-   blandit, congue felis vel, maximus risus. Etiam a nunc at nibh vestibulum posuere.\n\n`
+   blandit, congue felis vel, maximus risus. Etiam a nunc at nibh vestibulum posuere.`
 ]
 
 const textArea = document.querySelector('.output textarea');
@@ -98,7 +98,7 @@ function makeLorem(e) {
   if(arrNum == 0 || arrNum > 9 || arrNum < 0){
     textArea.textContent = loremArray[random];
   }else{
-    const arrayText = loremArray.slice(0, arrNum).join("");
+    const arrayText = loremArray.slice(0, arrNum).join("\n\n");
     textArea.textContent = arrayText;
   }
 }
