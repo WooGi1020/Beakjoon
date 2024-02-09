033a090fd83db6f853796c5dc8b841f953b9c72c
우기 <jungwk1020@gmail.com>
2024-02-09 20:23:26 +0900

    add .gitignore

--- Front-End-Prc/HTML+CSS+JS/My_Projects/FE_PROJECT_Exchange_Rater/script.js
+++ Front-End-Prc/HTML+CSS+JS/My_Projects/FE_PROJECT_Exchange_Rater/script.js
@@ -2,8 +2,6 @@ const amountInput = document.querySelector('.input-amount input');
 const selectInput = document.querySelector('.select-money select');
 const resultOutput = document.querySelector('.result-container p');
 
-const API_KEY = 'a79a246814ae04aa071d390e';
-
 function handleExchange(e){
   if(e.key === 'Enter'){
     const date = new Date();
