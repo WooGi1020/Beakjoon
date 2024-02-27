4966e1895276389b2128ba01e132b9c8316f2185
우기 <jungwk1020@gmail.com>
2024-02-27 22:01:49 +0900

    20240227

---
+++ Front-End-Prc/Node_React_Next_Study/Learn_React_Next/section6/components/SubLayout.js
@@ -0,0 +1,5 @@
+export default function SubLayout(children) {
+  return <div className="SubLayout">
+    {children}
+  </div>
+}
--- Front-End-Prc/Node_React_Next_Study/Learn_React_Next/section6/pages/search/index.js
+++ Front-End-Prc/Node_React_Next_Study/Learn_React_Next/section6/pages/search/index.js
@@ -1,3 +1,8 @@
+import SubLayout from "@/components/SubLayout"
+
 export default function Search() {
   return <div>Search Page</div>
 }
+
+// 모든 함수는 객체이므로, 점문법을 통해 Layout 프로퍼티에 SubLayout 컴포넌트를 할당한 것
+Search.Layout = SubLayout;
